54b5f88788052586ec65513f3b30f45a087af344
horladimeygee <56276810+horladimeygee@users.noreply.github.com>
2022-03-14 14:19:18 +0100

    Update accordion.js

--- components/accordion.js
+++ components/accordion.js
@@ -12,7 +12,7 @@ export default function Accordion({ q, active, onToggle   }) {
                 <div style={{ cursor: "pointer" }} className='flex justify-between items-center py-2 ' onClick={onToggle}>
                     <p className='font-black text-base'>{q.question}</p>
                     <span className='w-[30px] text-center  relative h-8 ml-2  ' >
-                        {active ? <Image className='absolute' src={arrowup} alt='arrowup' /> : <Image className='absolute' src={arrowdown} alt='arrowdown' />} 
+                        {active ? <Image src={arrowup} alt='arrowup' /> : <Image src={arrowdown} alt='arrowdown' />} 
                     </span>
                 </div>
                 <p className={`replywrapper ${active ? "open" : "" } font-[circularstd]  font-sm`}>{q.reply}</p>
