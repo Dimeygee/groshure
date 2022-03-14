d3ecd904334ceb4f67c2933702c82cba599cc378
horladimeygee <56276810+horladimeygee@users.noreply.github.com>
2022-03-14 14:40:51 +0100

    Update waitlist.js

--- components/waitlist.js
+++ components/waitlist.js
@@ -7,7 +7,7 @@ export default function WaitList({ width, boxShadow }) {
             boxShadow: boxShadow,
         }} className={`flex justify-between border-2 border-white bg-white p-1 min-h-[50px] rounded-full md:w-[${width}] `}>
               <form className='flex grow w-[100%] md:w-fit'>
-                <input style={{ border: "none" , outline: "none"}} class='grow md:text-18 text-sm w-[100%]
+                <input style={{ border: "none" , outline: "none"}} className='grow md:text-18 text-sm w-[100%]
                  md:w-fit py-1 md:px-8 px-2 rounded-full' type='text' placeholder="Enter email address" />
               </form>
               <Button backgroundColor="#FD4C5C" color="white" borderRadius="40px" paddingBottom="8px" paddingTop="8px" paddingRight="30px" paddingLeft="30px" fontSize="18px" flexGrow="1">
