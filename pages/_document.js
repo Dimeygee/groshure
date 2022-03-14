f0ec157ed112c16454b3246cb842504c7e186547
horladimeygee <56276810+horladimeygee@users.noreply.github.com>
2022-03-14 14:01:09 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,9 +7,9 @@ class MyDocument extends Document {
       <Html>
         <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap" rel="stylesheet" />
-        <link rel="preload" href="../public/font/circularstd.ttf"  as="font" crossorigin="true"/>
+        <link rel="preload" href="../public/font/circularstd.ttf"  as="font" crossOrigin="true"/>
         </Head>
         <body>
           <Main />
